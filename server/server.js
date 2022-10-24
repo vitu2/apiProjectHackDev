38ef345e3843142e6f9c06ddb5e6c488f1a4aa25
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2022-10-24 17:41:16 -0300

    api

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config({
+  path: "./.env",
+});
+
+const express = require("express");
+const server = express();
+const port = process.env.SERVER_PORT || 80;
+const cors = require("cors");
+
+
+server.use(cors());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use("/", require("./router/productsRouter"));
+server.use("/", require("./router/customersRouter"));
+server.use("/", require("./router/addressRouter"));
+server.use("/", require("./router/pedidosRouter"));
+server.use("/", require("./router/orderDetailsRouter"));
+server.use("/", require("./router/smsRouter"))
+
+server.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+server.listen(port, () => {
+  console.log(`servidor escutando na porta ${port}`);
+})
+
