b50e0c1ce253aeeeaadc64c84d73bace4ce14e54
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2022-10-24 19:46:49 -0300

    rotas do sms

--- server/data/smsData.js
+++ server/data/smsData.js
@@ -1,28 +1,28 @@
-require("dotenv").config({
-  path: ".././.env",
-});
+// require("dotenv").config({
+//   path: ".././.env",
+// });
 
-const accountSid = process.env.SID;
-const authToken = process.env.AUTHTOKEN;
+// const accountSid = process.env.SID;
+// const authToken = process.env.AUTHTOKEN;
 
-console.log(accountSid)
-console.log(authToken)
+// console.log(accountSid)
+// console.log(authToken)
 
-const client = require('twilio')(accountSid, authToken);
+// const client = require('twilio')(accountSid, authToken);
 
-const numeroZapZap = 'whatsapp:+556293176806';
+// const numeroZapZap = 'whatsapp:+556293176806';
 
-exports.getSmsData = async function (name, pedido_id) {
-  console.log(pedido_id)
-  console.log(name)
+// exports.getSmsData = async function (name, pedido_id) {
+//   console.log(pedido_id)
+//   console.log(name)
     
-    client.messages 
-.create({ 
-  body:  `Ola ${name} a compra foi finalizada com sucesso, Nº ${pedido_id}`, 
-  from: 'whatsapp:+14155238886',       
-  to: numeroZapZap    
-}) 
-.then(message => console.log('mensagem enviada',message.sid)) 
-.done();
-  };
+//     client.messages 
+// .create({ 
+//   body:  `Ola ${name} a compra foi finalizada com sucesso, Nº ${pedido_id}`, 
+//   from: 'whatsapp:+14155238886',       
+//   to: numeroZapZap    
+// }) 
+// .then(message => console.log('mensagem enviada',message.sid)) 
+// .done();
+//   };
 
--- server/service/smsService.js
+++ server/service/smsService.js
@@ -1,6 +1,6 @@
-const smsData = require("../data/smsData");
+// const smsData = require("../data/smsData");
 
-exports.getSmsData = function (name, pedido_id) {
-  return smsData.getSmsData(name, pedido_id);
-};
+// exports.getSmsData = function (name, pedido_id) {
+//   return smsData.getSmsData(name, pedido_id);
+// };
 
